a9a0279295b8404707a5c0bcfefefd8384f036b9
Matthew Kozlowski <Matthew.Kozlowski@dsst.local>
2019-03-05 12:23:11 -0700

    completed entry of quiz responses into datastore and began wireframing html

--- datastore.js
+++ datastore.js
@@ -1,3 +1,37 @@
 const STORE = [
-
+    {
+        question: 'What is a baby goat called?',
+        answers: [
+            'A baby', 'A youth', 'A kid', 'A child'
+        ],
+        correctAnswer: 'A kid'
+     },
+     {
+         question: 'Which of the following have domesticated goats not typically been used for?',
+         answers: [
+            'Hair', 'Meat', 'Milk', 'Horns'
+         ],
+         correctAnswer: 'Horns'
+     },
+    {
+        question: 'How many different breeds of goats are there?',
+        answers: [
+            '75', '150', '230', '300'
+        ],
+        correctAnswer: '300'
+    },
+    {
+        question: 'A female goat is called a:',
+        answers: [
+            'Doe', 'Jenny', 'Queen', 'Pen'
+        ],
+        correctAnswer: 'Doe'
+    },
+    {
+        question: 'The proper name for the YouTube famous fainting goats is:',
+        answers: [
+            'Boer', 'Myotonic', 'Anglo-Nubian', 'Pygora'
+        ],
+        correctAnswer: ''
+    }
 ];
