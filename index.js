6035cb234f26a3f0724a7c7de56165d07a8d0a8a
Matt <maekozlowski@gmail.com>
2019-03-19 16:28:48 -0600

    Added goat rating to end screen

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ function correctAnswerDisplay() {
     <h2>Correct!</h2>
     <h2>You Nailed it!</h2>
     <p>Maybe you're more goat than you first appear?</p>
-    <img src="https://edenhills.files.wordpress.com/2011/03/dsc_0219.jpg"  class="babyGoatEating">
+    <img src="https://edenhills.files.wordpress.com/2011/03/dsc_0219.jpg"  class="imageSizing">
 </div>
 <button type="button" class="moveOn">MoveOn</button>`
     } else if (overallScore === 2 && questionNumber < STORE.length) {
@@ -123,7 +123,7 @@ function correctAnswerDisplay() {
         <h2>Correct!</h2>
         <h2>A kid no more!</h2>
         <p>That's two correct! Well done!</p>
-        <img src="https://i.imgur.com/XC5UeJr.jpg" class="babyGoatEating">
+        <img src="https://i.imgur.com/XC5UeJr.jpg" class="imageSizing">
     </div>
     <button type="button" class="moveOn">MoveOn</button>`
     } else if (overallScore === 3 && questionNumber < STORE.length) {
@@ -131,7 +131,7 @@ function correctAnswerDisplay() {
         <h2>Correct!</h2>
         <h2>Okay, so you think you can goat, huh?</h2>
         <p>Well, so far you can!</p>
-        <img src="https://i.imgur.com/RoLCztT.jpg" class="babyGoatEating">
+        <img src="https://i.imgur.com/RoLCztT.jpg" class="imageSizing">
     </div>
     <button type="button" class="moveOn">MoveOn</button>`
     } else if (overallScore === 4 && questionNumber < STORE.length) {
@@ -139,7 +139,7 @@ function correctAnswerDisplay() {
         <h2>Correct!</h2>
         <h2>Goat-tacular!!</h2>
         <p>That's four right, but who's counting?</p>
-        <img src="http://static1.squarespace.com/static/539dffebe4b080549e5a5df5/53fcdceee4b0b801964aedb1/5a9ef692e2c483767c8ecb0b/1524340523771/beverage-cocktail-decorative-napkins-sawyer-goat-museum-outlets.jpg" class="babyGoatEating">
+        <img src="http://static1.squarespace.com/static/539dffebe4b080549e5a5df5/53fcdceee4b0b801964aedb1/5a9ef692e2c483767c8ecb0b/1524340523771/beverage-cocktail-decorative-napkins-sawyer-goat-museum-outlets.jpg" class="imageSizing">
     </div>
     <button type="button" class="moveOn">MoveOn</button>`
     } else if (overallScore === 5) {
@@ -147,7 +147,7 @@ function correctAnswerDisplay() {
         <h2>Correct!</h2>
         <h2>Wow.</h2>
         <p>That's...that's a perfect score.</p>
-        <img src="https://s3.amazonaws.com/file.imleagues/Images/Teams/Uploaded/201802/201822142647906dafc8cf55e71f60e768dee7a828b291c.jpg" class="babyGoatEating">
+        <img src="https://s3.amazonaws.com/file.imleagues/Images/Teams/Uploaded/201802/201822142647906dafc8cf55e71f60e768dee7a828b291c.jpg" class="imageSizing">
     </div>
     <button type="button" class="seeResults">See Results!</button>`
     } else if (questionNumber === STORE.length) {
@@ -155,7 +155,7 @@ function correctAnswerDisplay() {
         <h2>Correct!</h2>
         <h2>Way to finish strong!</h2>
         <p>Let's see how you did!</p>
-        <img src="http://www.adobeacres.net/IMG_1499.JPG" class="babyGoatEating">
+        <img src="http://www.adobeacres.net/IMG_1499.JPG" class="imageSizing">
     </div>
     <button type="button" class="seeResults">See Results!</button>`
     }
@@ -175,7 +175,7 @@ function incorrectAnswerDisplay() {
         <h2>Oh No! You got that one wrong!</h2>
         <p>It's okay most KIDS don't know that one anyway ;)</p>
         <p>The correct answer was: ${STORE[questionNumber-1].correctAnswer}.</p>
-        <img src="https://i.imgflip.com/1bclgj.jpg">
+        <img src="https://i.redd.it/dr5obwe06d811.png" class="imageSizing">
     </div>
     <button type="button" class="moveOn">MoveOn</button>`
     } else if  (numberIncorrect === 2 && questionNumber < STORE.length) {
@@ -184,7 +184,7 @@ function incorrectAnswerDisplay() {
         <h2>You got that one wrong?</h2>
         <p>I feel like you should have been able to use process of elimination there.</p>
         <p>The correct answer was: ${STORE[questionNumber-1].correctAnswer}. You lemon, you.</p>
-        <img src="https://i0.wp.com/lh4.googleusercontent.com/-oG3nrZqC7BI/UZA-IiB136I/AAAAAAAAJDs/yZ-JD8v6l0A/s640/blogger-image--1256441326.jpg?ssl=1">
+        <img src="https://i0.wp.com/lh4.googleusercontent.com/-oG3nrZqC7BI/UZA-IiB136I/AAAAAAAAJDs/yZ-JD8v6l0A/s640/blogger-image--1256441326.jpg?ssl=1" class="imageSizing">
     </div>
     <button type="button" class="moveOn">MoveOn</button>`
     } else if (numberIncorrect === 3 && questionNumber < STORE.length) {
@@ -193,7 +193,7 @@ function incorrectAnswerDisplay() {
         <h2>Okay, so a passing grade is now unobtainable.</h2>
         <p>You may use Google now.</p>
         <p>The correct answer was: ${STORE[questionNumber-1].correctAnswer}.</p>
-        <img src="https://images.rapgenius.com/24e49e891db44863cfb55d11dc31bd83.1000x666x1.jpg">
+        <img src="https://images.rapgenius.com/24e49e891db44863cfb55d11dc31bd83.1000x666x1.jpg" class="imageSizing">
     </div>
     <button type="button" class="moveOn">MoveOn</button>`
     } else if (numberIncorrect === 4 && questionNumber < STORE.length) {
@@ -203,7 +203,7 @@ function incorrectAnswerDisplay() {
         <p>and you had Google.<p>
         <p>Did the doe one get you?</p>
         <p>The correct answer was: ${STORE[questionNumber-1].correctAnswer}.</p>
-        <img src="https://media-cdn.tripadvisor.com/media/photo-s/01/44/db/96/doe-a-deer-a-female-deer.jpg">
+        <img src="https://media-cdn.tripadvisor.com/media/photo-s/01/44/db/96/doe-a-deer-a-female-deer.jpg" class="imageSizing">
     </div>
     <button type="button" class="moveOn">MoveOn</button>`
     } else if (numberIncorrect === 5) {
@@ -213,7 +213,7 @@ function incorrectAnswerDisplay() {
         <p>Some teachers award full credit if you can do that!</p>  
         <p>I don't.</p>
         <p>The correct answer was: ${STORE[questionNumber-1].correctAnswer}.</p>
-        <img src="https://www.crowdfunder.co.uk/uploads/projects/14789.png?1424100369">
+        <img src="https://www.crowdfunder.co.uk/uploads/projects/14789.png?1424100369" class="imageSizing">
     </div>
     <button type="button" class="seeResults">See Results!</button>`
     } else if (questionNumber === STORE.length) {
@@ -222,7 +222,7 @@ function incorrectAnswerDisplay() {
         <h2>Well you got the last one wrong.</h2>
         <p>Don't let it keep you down though!</p>
         <p>The correct answer was: ${STORE[questionNumber-1].correctAnswer}.</p>
-        <img src="http://4.bp.blogspot.com/-exO_s6VltRQ/T1Yhjd-NYPI/AAAAAAAAF68/kHKJk0DNzS0/s1600/Screen+shot+2012-03-06+at+7.36.03+AM.png">
+        <img src="http://4.bp.blogspot.com/-exO_s6VltRQ/T1Yhjd-NYPI/AAAAAAAAF68/kHKJk0DNzS0/s1600/Screen+shot+2012-03-06+at+7.36.03+AM.png" class="imageSizing">
     </div>
     <button type="button" class="seeResults">See Results!</button>`
     }
@@ -236,6 +236,8 @@ function renderEndScreen() {
 }
 
 function createEndScreen() {
+    $('header').addClass('hide');
+    generateGoatRating();
     return `<div class="quizEndScreen">
     <h1>Overall Score: ${overallScore}</h1>
     <h2>Goat Rating: ${goatRating[0]} - ${goatRating[1]}</h2>
@@ -252,14 +254,27 @@ function restartQuiz() {
 function generateGoatRating() {
     if (overallScore === 0) {
         goatRating.push("0%");
-        goatRating.push("You are no goat!")
-    } //else if (overallScore === 1) {
-        //goatRating.push("20%")
-        //goatRating.push("Even cats are more goat than you are!")
-    //}
+        goatRating.push("You are no goat!");
+    } else if (overallScore === 1) {
+        goatRating.push("20%");
+        goatRating.push("Even cats are more goat than you are!");
+    } else if (overallScore === 2) {
+        goatRating.push("40%");
+        goatRating.push("More goat than the average bear, but that isn't saying too much");
+    } else if (overallScore === 3) {
+        goatRating.push("60%");
+        goatRating.push("Okay, so maybe you were raised on a farm?");
+    } else if (overallScore === 4) {
+        goatRating.push("80%");
+        goatRating.push("Dang! Are you actually a goat and just misclicked with a hoof?");
+    } else if (overallScore === 5) {
+        goatRating.push("100%");
+        goatRating.push("You aren't just the goat. You're the G.O.A.T");
+    } 
+
 }
 
-generateGoatRating();
+
 restartQuiz();
 renderEndScreen();
 checkAnswer();
